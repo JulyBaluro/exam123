25ae7e32cd9248756edd8cb9140451a7560e040f
JulyBaluro <julybaluro0@gmail.com>
2026-08-07 21:54:36 +0800

    new july

---
+++ app/Http/Controllers/RoomexamController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\roomexam;
+use Illuminate\Http\Request;
+
+class RoomexamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(roomexam $roomexam)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(roomexam $roomexam)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, roomexam $roomexam)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(roomexam $roomexam)
+    {
+        //
+    }
+}
